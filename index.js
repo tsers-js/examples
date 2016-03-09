648efa0eca50aec5099365d9cc187b633f14f9f3
Matti Lankinen <m.lankinen@iki.fi>
2016-03-10 01:25:36 +0200

    Add example playground with router

--- index.js
+++ index.js
@@ -1,2 +1,74 @@
+import {Observable as O} from "rx"
+import TSERS from "@tsers/core"
+import makeReactDOM from "@tsers/react"
+import makeRouter from "./router-driver"
 
-const MSG = "tsers"
+
+import hello from "./01-hello-world"
+
+
+const main = T => in$ => {
+  const {Router: {route}} = T
+
+  return route(in$, "Router", {
+    "/hello": playground(T, hello),
+    "/*": navigation(T)
+  })
+}
+
+const navigation = T => in$ => {
+  const {DOM: {withEvents, events, h}, compose} = T
+  return intent(view())
+
+  function view() {
+    const vdom = h("div", [
+      h("h1", "Example list"),
+      h("ul.examples", [
+        h("li", [h("a", {href: "#hello"}, "Hello World")])
+      ])
+    ])
+
+    return withEvents(O.just(vdom))
+  }
+
+  function intent(vdom$) {
+    const route$ = events(vdom$, ".examples a", "click")
+      .do(e => e.preventDefault())
+      .map(e => e.target.getAttribute("href"))
+
+    return compose({DOM: vdom$, Router: route$})
+  }
+}
+
+const playground = (T, example) => in$ => {
+  const {DOM: {withEvents, events, h}, compose, run} = T
+  return intent(view())
+
+  function view() {
+    const {DOM: exampleDom$, ...exampleOut} = run(in$, example(T))
+    const vdom$ = exampleDom$.map(vdom =>
+      h("div", [
+        h("div", [h("a.back-to-examples", {href: "#"}, "Back to examples")]),
+        h("div", [vdom])
+      ]))
+
+    return [withEvents(vdom$), exampleOut]
+  }
+
+  function intent([vdom$, {Router, ...out}]) {
+    const route$ = events(vdom$, ".back-to-examples", "click")
+      .do(e => e.preventDefault())
+      .map(() => e.target.getAttribute("href"))
+      .merge(Router)
+
+    return compose({DOM: vdom$, Router: route$, ...out})
+  }
+}
+
+
+const [T, S, E] = TSERS({
+  DOM: makeReactDOM("#app"),
+  Router: makeRouter()
+})
+
+E(T.loop(S, main(T)))
---
+++ router-driver/index.js
@@ -0,0 +1,55 @@
+import Router from "routes"
+import {Observable as O} from "rx"
+import {createHashHistory} from "history"
+
+const isObj = x => typeof x === "object" && x.constructor === Object
+
+function makeRouterDriver(h = createHashHistory()) {
+  return function RouterDriver({loop, from, compose}) {
+
+    const location$ = O.create(o => {
+      const dispose = h.listen(loc => o.onNext(loc))
+      return {dispose}
+    }).shareReplay(1)
+
+    function route(in$, driverName, spec) {
+      const r = new Router()
+      Object.keys(spec).forEach(route => {
+        r.addRoute(route, spec[route])
+      })
+
+      const path$ = from(in$, driverName).map(l => l.pathname)
+      return path$.flatMapLatest(path => {
+        const res = r.match(path)
+        if (!res || !res.fn) {
+          return O.throw(new Error("No route found for path: " + path))
+        }
+        const main = res.fn
+        const params$ = compose({[`${driverName}.params`]: O.just(res.params)})
+        return loop(in$.merge(params$), main)
+      })
+    }
+
+    function executor(location$) {
+      return location$.subscribe(newPath => {
+        console.log(newPath)
+        h.push(isObj(newPath) ? newPath : {
+          pathname: newPath
+        })
+      })
+    }
+
+    const transducers = {
+      route
+    }
+
+    return {
+      signals: location$,
+      transducers,
+      executor
+    }
+  }
+
+}
+
+export default makeRouterDriver
