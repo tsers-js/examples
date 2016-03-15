f602d14a27f8654afd2c0f449974cbcde8f9d4c1
Matti Lankinen <m.lankinen@iki.fi>
2016-03-15 02:56:23 +0200

    Simplify hello world example

--- 01-hello-world/index.js
+++ 01-hello-world/index.js
@@ -1,48 +1,34 @@
 import {Observable as O} from "rx"
 
 const main = T => in$ => {
-  const {DOM: {h, withEvents}, decompose, compose} = T
+  const {DOM: {h, withEvents, events}, decompose, compose} = T
 
-  const [actions] = decompose(in$, "Seconds")
+  const [actions] = decompose(in$, "add$")
   return intent(view(model(actions)))
 
-  function model({Seconds: sec$}) {
-    const msg$ = sec$.map(sec => "Tsers" + (sec % 2 ? "." : "!"))
+  function model({add$}) {
+    const msg$ = add$
+      .map(() => "!")
+      .startWith("Tsers")
+      .scan((acc, s) => acc + s)
     return msg$
   }
 
   function view(msg$) {
     const vdom$ = msg$.map(msg =>
       h("div", [
-        h("h1", msg)
+        h("h1", msg),
+        h("button.add", "Click me!")
       ]))
     return withEvents(vdom$)
   }
 
   function intent(vdom$) {
-    return compose({DOM: vdom$})
+    const add$ = events(vdom$, ".add", "click")
+    const loop$ = compose({add$})
+    const out$ = compose({DOM: vdom$})
+    return [out$, loop$]
   }
 }
 
 export default main
-
-/*
-// How to run your application
-// indexOfYourApp.js
-import {Observable as O} from "rx"
-import TSERS from "@tsers/core"
-import makeDOM from "@tsers/react"
-import makeSeconds from "./seconds-driver"
-
-import main from "./hello-world"
-
-
-const [Transducers, signal$, execute] = TSERS({
-  DOM: makeDOM("#app"),
-  Seconds: makeSeconds()
-})
-
-const {run} = Transducers
-execute(run(singal$, main(Transducers)))
-
- */
