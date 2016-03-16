14abaf5b4e9ba82b8c0e4de7cd2407d57c55ec6e
Matti Lankinen <m.lankinen@iki.fi>
2016-03-16 22:00:21 +0200

    Simplify naive nested counters

--- basic-naive-nested-counters/index.js
+++ basic-naive-nested-counters/index.js
@@ -3,11 +3,11 @@ import {Observable as O} from "rx"
 import Counter from "../basic-counter"
 
 const main = (T, initial = {a: 0, b: 5}) => in$ => {
-  const {DOM: {h}, run, compose, decompose} = T
+  const {DOM: {h}, run, compose, extract} = T
 
-  const [a] = decompose(run(in$, Counter(T)), "DOM")
-  const [b] = decompose(run(in$, Counter(T)), "DOM")
-  const vdom$ = O.combineLatest(a.DOM, b.DOM, (aDOM, bDOM) =>
+  const aDOM$ = extract(run(in$, Counter(T)), "DOM")
+  const bDOM$ = extract(run(in$, Counter(T)), "DOM")
+  const vdom$ = O.combineLatest(aDOM$, bDOM$, (aDOM, bDOM) =>
     h("div", [
       aDOM,
       h("hr"),
