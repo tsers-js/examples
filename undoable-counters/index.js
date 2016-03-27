df4547bdd7f089e1d5b78e3f8292f29fa06a0d60
Matti Lankinen <m.lankinen@iki.fi>
2016-03-27 19:36:10 +0300

    Add some docs to undo-redo

--- undoable-counters/index.js
+++ undoable-counters/index.js
@@ -7,6 +7,10 @@ import undoable from "./undoable"
 export default function main(signals) {
   const {DOM, model$, demux, mux} = signals
   const {h} = DOM
+
+  // Using our undoable is dead-simple: just pass the model you want to make
+  // undoable and undoable will return a new model (value$) and some helper
+  // mods / observables that can be used to control the undo/redo
   const {value$: counters$, canUndo$, canRedo$, mods: {undo$, redo$}} = undoable(model$)
 
   const counterList = CounterList({...signals, model$: counters$})
@@ -22,7 +26,8 @@ export default function main(signals) {
         ])))
 
   // undo$ and redo$ are already converted by using model$.mod so there is
-  // no need to do that again 
+  // no need to do that again. Now we only want to trigger them every time
+  // when either undo or redo button is clicked
   const mod$ = O.merge(
     undo$.sample(DOM.events(vdom$, ".undo", "click")),
     redo$.sample(DOM.events(vdom$, ".redo", "click"))
