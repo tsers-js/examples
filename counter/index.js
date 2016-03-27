1ffed7cf209e13d85e8238b41ca9d3e7d9262f65
Matti Lankinen <m.lankinen@iki.fi>
2016-03-27 17:40:09 +0300

    Add documentation to counter example

--- counter/index.js
+++ counter/index.js
@@ -4,7 +4,12 @@ import {Observable as O} from "rx"
 export default function main({DOM, model$, mux}) {
   const {h} = DOM
 
-  // we can use our model like normal observable
+  // model$ is an instance of @tsers/model - it provides the application
+  // state as an observable, so you can use model$ like any other observable
+  // (map, filter, combineLatest, ...).
+
+  // let's use the model$ observable to get its value and render a virtual-dom
+  // based on the value
   const vdom$ = DOM.prepare(model$.map(counter =>
     h("div", [
       h("h1", `Counter value is ${counter}`),
@@ -12,15 +17,44 @@ export default function main({DOM, model$, mux}) {
       h("button.dec", "--")
     ])))
 
+  // model$ enables you to change the state by emitting "modify functions"
+  // as out output signals. The modify functions have always signature
+  // (curState => nextState) - they receive the current state of the model
+  // as input and must provide the next state based on the current state
+
+  // Let's make modify functions for the counter: when increment button is
+  // clicked, increment the counter state by +1. When decrement button is clicked,
+  // decrement the state by -1
   const incMod$ = DOM.events(vdom$, ".inc", "click").map(() => state => state + 1)
   const decMod$ = DOM.events(vdom$, ".dec", "click").map(() => state => state - 1)
 
-  // let's merge all mods from this component
+  // And because the modify are just observables, we can merge them
   const mod$ = O.merge(incMod$, decMod$)
 
   return mux({
     DOM: vdom$,
-    // and make the compatible with the model
+    // Like DOM interpreter, Model interpreter also expects that model modifications
+    // are "prepared". That's why model interpreter provides a helper function .mod(mod$)
+    // which converts the modify function observables to actual state modify observables
+    // that do the actual state modifications
     model$: model$.mod(mod$)
   })
 }
+
+
+/*
+
+// In order to run this app, you must setup a model interpreter
+// for counter (with initial value):
+import TSERS from "@tsers/core"
+import ReactDOM from "@tsers/react"
+import Model from "@tsers/model"
+
+import Counter from "./counter"
+
+TSERS(Counter, {
+  DOM: ReactDOM("#app"),
+  model$: Model(0)          // initial value = 0
+})
+
+*/
